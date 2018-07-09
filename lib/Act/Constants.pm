efd92b07212028cbdc5ee91ed86f32fac7a81dcd
Thomas Bloor <tom@tbsliver.me>
2018-07-09 11:39:37 +0100

    Create mod_perl2 constants switch for

--- lib/Act/Constants.pm
+++ lib/Act/Constants.pm
@@ -5,23 +5,29 @@ use warnings;
 
 use Exporter qw/ import /;
 
-use Apache::Constants ();
-
 my $CONSTANTS;
 
 BEGIN {
+  my $mp2 = ( exists $ENV{MOD_PERL_API_VERSION} and
+                $ENV{MOD_PERL_API_VERSION} >= 2 );
+  if ( $mp2 ) {
+    require Apache2::Const;
+  } else {
+    require Apache::Constants;
+  }
   $CONSTANTS = {
-    OK              => Apache::Constants::OK(),
-    DECLINED        => Apache::Constants::DECLINED(),
-    DONE            => Apache::Constants::DONE(),
-    NOT_FOUND       => Apache::Constants::NOT_FOUND(),
-    FORBIDDEN       => Apache::Constants::FORBIDDEN(),
-    AUTH_REQUIRED   => Apache::Constants::AUTH_REQUIRED(),
-    SERVER_ERROR    => Apache::Constants::SERVER_ERROR(),
-    HTTP_NO_CONTENT => Apache::Constants::HTTP_NO_CONTENT(),
-    BAD_REQUEST     => Apache::Constants::BAD_REQUEST(),
-    M_GET           => Apache::Constants::M_GET(),
-    REDIRECT        => Apache::Constants::REDIRECT(),
+    MP2             => $mp2,
+    OK              => $mp2 ? Apache2::Const::OK()                : Apache::Constants::OK(),
+    DECLINED        => $mp2 ? Apache2::Const::DECLINED()          : Apache::Constants::DECLINED(),
+    DONE            => $mp2 ? Apache2::Const::DONE()              : Apache::Constants::DONE(),
+    NOT_FOUND       => $mp2 ? Apache2::Const::HTTP_NOT_FOUND()    : Apache::Constants::NOT_FOUND(),
+    FORBIDDEN       => $mp2 ? Apache2::Const::FORBIDDEN()         : Apache::Constants::FORBIDDEN(),
+    AUTH_REQUIRED   => $mp2 ? Apache2::Const::AUTH_REQUIRED()     : Apache::Constants::AUTH_REQUIRED(),
+    SERVER_ERROR    => $mp2 ? Apache2::Const::SERVER_ERROR()      : Apache::Constants::SERVER_ERROR(),
+    HTTP_NO_CONTENT => $mp2 ? Apache2::Const::HTTP_NO_CONTENT()   : Apache::Constants::HTTP_NO_CONTENT(),
+    BAD_REQUEST     => $mp2 ? Apache2::Const::HTTP_BAD_REQUEST()  : Apache::Constants::BAD_REQUEST(),
+    M_GET           => $mp2 ? Apache2::Const::M_GET()             : Apache::Constants::M_GET(),
+    REDIRECT        => $mp2 ? Apache2::Const::REDIRECT()          : Apache::Constants::REDIRECT(),
   }
 }
 
